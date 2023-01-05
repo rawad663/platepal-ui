ed6f686787eded00a86ed16b3ee9f87f40003483
rkaram <rkaram@shutterstock.com>
2023-01-05 18:05:52 +0200

    feat: add google analytics

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -2,4 +2,9 @@ import { dev, prod } from '@root/config';
 
 const { NODE_ENV } = process.env;
 
-export const getConfig = () => (NODE_ENV === 'production' ? prod : dev);
+export const ENV = {
+  production: 'production',
+  dev: 'development',
+};
+
+export const getConfig = () => (NODE_ENV === ENV.production ? prod : dev);
